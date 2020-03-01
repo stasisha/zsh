a83d2663a5640e24d40e64f41de1957401543cd7
stasisha <stasisha@users.noreply.github.com>
2020-03-01 13:16:28 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#xcode install 
+xcode-select —-install
+
+#brew install 
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew cask install iterm2
+
+brew install zsh
+
+#oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
