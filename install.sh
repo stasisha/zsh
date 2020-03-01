8571bbe6b2ac5c0183a2f7768c69463d0c6c6dfe
stasisha <stasisha@users.noreply.github.com>
2020-03-01 15:39:48 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,20 @@
-#xcode install 
-xcode-select —-install
+#!/bin/bash
 
-#brew install 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-brew cask install iterm2
-
-brew install zsh
+addLineIfNotExists() {
+  local LINE=$1
+  local FILE=$2
+  sudo grep -qF -- "$LINE" "$FILE" || echo "$LINE" | sudo tee -a "$FILE"
+}
 
 #oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+rm -rf ~/.oh-my-zsh/custom/themes/powerlevel9k
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+sed -i '' '/### appearance start ###/,/### appearance end ###/d' ~/.zshrc
+addLineIfNotExists "### appearance start ###" ~/.zshrc
+addLineIfNotExists "source ~/.appearance" ~/.zshrc
+addLineIfNotExists "### appearance end ###" ~/.zshrc
+
+source ~/.zshrc
