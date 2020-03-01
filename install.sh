553eafee7154f266343c32620461514d22ef495f
stasisha <stasisha@users.noreply.github.com>
2020-03-01 21:42:45 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 source /dev/stdin <<< "$(curl --insecure https://raw.githubusercontent.com/stasisha/bash-utils/master/file-edit.sh)";
+source /dev/stdin <<< "$(curl --insecure https://raw.githubusercontent.com/stasisha/bash-utils/master/brew.sh";
 
-brew install fzf
+brew-install-if-not-installed fzf
 
-rm -rf ~/.oh-my-zsh/custom/themes/powerlevel9k
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+if [ -d "~/.oh-my-zsh/custom/themes/powerlevel9k" ]; then
+    git -C ~/.oh-my-zsh/custom/themes/powerlevel9k pull
+else
+    git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+fi
 
 curl -L https://raw.githubusercontent.com/stasisha/zsh/master/.appearance -o ~/.appearance
 
-#oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-addLineBeforeLineIfNotExists "source ~/.appearance" "source $ZSH/oh-my-zsh.sh" ~/.zshrc
+addLineBeforeLineIfNotExists "source ~/.appearance" "source \$ZSH/oh-my-zsh.sh" ~/.zshrc
 
 source ~/.zshrc
