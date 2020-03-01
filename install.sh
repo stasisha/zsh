9a9276a55956f40bf7a93df803744514be98bc34
stasisha <stasisha@users.noreply.github.com>
2020-03-01 16:27:09 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-addLineIfNotExists() {
+addLineToTopIfNotExists() {
   local LINE=$1
   local FILE=$2
-  sudo grep -qF -- "$LINE" "$FILE" || echo "$LINE" | sudo tee -a "$FILE"
+  sudo grep -qF -- "$LINE" "$FILE" || echo -e  "$LINE""\n$(cat todo.txt)" > todo.txt
 }
+
+brew install fzf
 
 rm -rf ~/.oh-my-zsh/custom/themes/powerlevel9k
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
 curl -L https://raw.githubusercontent.com/stasisha/zsh/master/.appearance -o ~/.appearance
 
-sed -i '' '/### appearance start ###/,/### appearance end ###/d' ~/.zshrc
-addLineIfNotExists "### appearance start ###" ~/.zshrc
-addLineIfNotExists "source ~/.appearance" ~/.zshrc
-addLineIfNotExists "### appearance end ###" ~/.zshrc
+addLineToTopIfNotExists "source ~/.appearance" ~/.zshrc
 
 #oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
