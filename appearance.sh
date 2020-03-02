39b57438d3113d64f21dad7dfb69af128706870b
stasisha <stasisha@users.noreply.github.com>
2020-03-02 22:16:55 +0200

    Update appearance.sh

--- appearance.sh
+++ appearance.sh
@@ -5,12 +5,6 @@ source /dev/stdin <<< "$(curl -fsSL https://raw.githubusercontent.com/stasisha/b
 
 brew-install-if-not-installed "fzf"
 
-if [ -d "~/.oh-my-zsh/custom/themes/powerlevel9k" ]; then
-    git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-else
-    git -C ~/.oh-my-zsh/custom/themes/powerlevel9k pull
-fi
-
 if [ -d "${ZSH_CUSTOM}/themes/powerlevel10k" ]; then
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 else
