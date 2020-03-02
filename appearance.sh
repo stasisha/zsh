fcfd2fc13746bacb9b4aaf5f8aade985846e4fd6
stasisha <stasisha@users.noreply.github.com>
2020-03-02 16:53:49 +0200

    Create appearance.sh

---
+++ appearance.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+
+source /dev/stdin <<< "$(curl -fsSL https://raw.githubusercontent.com/stasisha/bash-utils/master/file-edit.sh)";
+
+removeLine 'source $ZSH\/oh-my-zsh.sh' ~/.zshrc
+addLineToBottomIfNotExists 'source ~/.appearance' ~/.zshrc
+addLineToBottomIfNotExists 'source $ZSH/oh-my-zsh.sh' ~/.zshrc
+
+echo "Select DroidSansMono Nerd Font in iterm2 Preferences."
