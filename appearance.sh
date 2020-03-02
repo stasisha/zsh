c232b6ce170e6bf8f4694c424f296502e74f2077
stasisha <stasisha@users.noreply.github.com>
2020-03-03 00:00:12 +0200

    Update appearance.sh

--- appearance.sh
+++ appearance.sh
@@ -2,12 +2,6 @@
 
 source /dev/stdin <<< "$(curl -fsSL https://raw.githubusercontent.com/stasisha/bash-utils/master/file-edit.sh)";
 
-if [ -d "${ZSH_CUSTOM}/themes/powerlevel10k" ]; then
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
-else
-    git -C ~ZSH_CUSTOM/themes/powerlevel10k pull
-fi
-
 curl -L https://raw.githubusercontent.com/stasisha/zsh/master/.appearance.sh -o ~/.appearance.sh
 
 removeLine 'source $ZSH\/oh-my-zsh.sh' ~/.zshrc
