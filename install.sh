fde54ae0e0e53f65b2cb94c86efcc94e062e63f0
stasisha <stasisha@users.noreply.github.com>
2020-03-02 23:09:55 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+source /dev/stdin <<< "$(curl -fsSL https://raw.githubusercontent.com/stasisha/bash-utils/master/brew.sh)";
+
+brew-install-if-not-installed "fzf"
+
+cd ~/Library/Fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
+cd -
+
 #oh-my-zsh
 if [ -d "~/.oh-my-zsh" ]; then
     git -C ~/.oh-my-zsh pull
