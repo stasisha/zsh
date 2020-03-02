b5fc16e574600b1ace4d44c3c858e51c8ab12bbe
stasisha <stasisha@users.noreply.github.com>
2020-03-02 21:54:27 +0200

    Update appearance.sh

--- appearance.sh
+++ appearance.sh
@@ -12,7 +12,7 @@ else
 fi
 
 if [ -d "${ZSH_CUSTOM}/themes/powerlevel10k" ]; then
-    git -C ~/powerlevel10k pull
+    git -C ~ZSH_CUSTOM/themes/powerlevel10k pull
 else
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 fi
