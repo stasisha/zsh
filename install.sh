94654bd656566e63e4fc0c41b058018e86c1f00b
stanislav-bilov <34650104+stanislav-bilov@users.noreply.github.com>
2020-03-16 15:30:44 +0200

    Update and rename install-osx.sh to install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ source /dev/stdin <<< "$(curl -fsSL https://raw.githubusercontent.com/stasisha/b
 
 brew-install-if-not-installed "fzf"
 
+mkdir ~/Library/Fonts
 cd ~/Library/Fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
 cd -
 
